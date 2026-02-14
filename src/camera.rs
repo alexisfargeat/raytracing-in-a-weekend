03cd02452ce46fc6829e2408149455316934e0f3
Alexis Fargeat <alexis.fargeat@gmail.com>
2026-02-14 14:31:07 +0100

    refactor: move main code to camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,9 @@
 use crate::file::ImageParameters;
+use crate::file::write_image_to_file;
+use crate::objects::ObjectList;
+use crate::ray::Ray;
+use crate::utils::Interval;
+use crate::vec3::Color;
 use crate::vec3::Point3;
 use crate::vec3::Vec3;
 
@@ -45,9 +50,43 @@ impl Camera {
             - self.viewport_v() / 2.0
     }
 
-    pub fn pixel_center(&self, x: usize, y: usize, image_params: &ImageParameters) -> Point3 {
+    fn pixel_center(&self, x: usize, y: usize, image_params: &ImageParameters) -> Point3 {
         self.viewport_upper_left()
-            + x as f64 * self.delta_u(image_params)
-            + y as f64 * self.delta_v(image_params)
+            + (x as f64 + 0.5) * self.delta_u(image_params)
+            + (y as f64 + 0.5) * self.delta_v(image_params)
+    }
+
+    pub fn render(
+        &self,
+        world: &ObjectList,
+        image_params: &ImageParameters,
+    ) -> std::io::Result<()> {
+        pub fn color(normal_vector: Vec3) -> Color {
+            0.5 * Color {
+                x: normal_vector.x + 1.0,
+                y: normal_vector.y + 1.0,
+                z: normal_vector.z + 1.0,
+            }
+        }
+
+        let color_function = |x: usize, y: usize| -> Color {
+            let ray_direction: Vec3 = self.pixel_center(x, y, image_params) - self.center;
+
+            let ray = Ray {
+                origin: self.center,
+                direction: ray_direction,
+            };
+
+            let hit = world.hit(&ray, Interval::new(0.0, f64::MAX));
+
+            match hit {
+                None => ray.color(),
+                Some(hit_record) => color(hit_record.normal),
+            }
+        };
+
+        write_image_to_file("test.ppm", image_params, color_function)?;
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use rt::camera::Camera;
 use rt::file::ImageParameters;
-use rt::file::write_image_to_file;
 use rt::objects::ObjectList;
 use rt::objects::sphere::Sphere;
-use rt::ray::Ray;
-use rt::utils::Interval;
-use rt::vec3::Color;
 use rt::vec3::Point3;
-use rt::vec3::Vec3;
 
 fn main() -> std::io::Result<()> {
     // image constants
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: usize = 800;
     const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
+
+    let image_parameters = ImageParameters {
+        width: IMAGE_WIDTH,
+        height: IMAGE_HEIGHT,
+    };
 
     // camera
     const VIEWPORT_HEIGHT: f64 = 2.0;
@@ -27,11 +27,6 @@ fn main() -> std::io::Result<()> {
         focal_length: 1.0,
         viewport_width: VIEWPORT_WIDTH,
         viewport_height: VIEWPORT_HEIGHT,
-    };
-
-    let image_parameters = ImageParameters {
-        width: IMAGE_WIDTH,
-        height: IMAGE_HEIGHT,
     };
 
     let mut world: ObjectList = ObjectList::default();
@@ -56,31 +51,7 @@ fn main() -> std::io::Result<()> {
         radius: 100.0,
     });
 
-    pub fn color(normal_vector: Vec3) -> Color {
-        0.5 * Color {
-            x: normal_vector.x + 1.0,
-            y: normal_vector.y + 1.0,
-            z: normal_vector.z + 1.0,
-        }
-    }
-
-    let color_function = |x: usize, y: usize| -> Color {
-        let ray_direction: Vec3 = camera.pixel_center(x, y, &image_parameters) - camera.center;
-
-        let ray = Ray {
-            origin: camera.center,
-            direction: ray_direction,
-        };
-
-        let hit = world.hit(&ray, Interval::new(0.0, f64::MAX));
-
-        match hit {
-            None => ray.color(),
-            Some(hit_record) => color(hit_record.normal),
-        }
-    };
-
-    write_image_to_file("test.ppm", &image_parameters, color_function)?;
+    camera.render(&world, &image_parameters)?;
 
     Ok(())
 }
